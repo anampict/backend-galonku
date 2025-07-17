d0485f57605d499ff89b7aa1a4e7cb3d479ec2ef
anampict <anampict@gmail.com>
2025-07-17 20:51:40 +0700

    membuat models user

---
+++ models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID 	  primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Email string             `bson:"email" json:"email"`
+	Password string          `bson:"password" json:"-"`
+	Role  string             `bson:"role" json:"role"` // "admin" atau "user"
+}
