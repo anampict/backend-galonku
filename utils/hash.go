77182d84f2720aa25833283103ed8996cda2ffc9
anampict <anampict@gmail.com>
2025-07-17 22:12:56 +0700

    menambahkan fungsi untuk cek apakah password yang di input dan yang sudah di hash sudah sesuai apa belum

--- utils/hash.go
+++ utils/hash.go
@@ -8,3 +8,10 @@ func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+//fungsi untuk membandingkan password yang diinput dengan password yang sudah di-hash
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
