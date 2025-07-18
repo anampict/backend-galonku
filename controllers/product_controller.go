f5820dff57350bcf02cb859d562bd9bf526c47bc
anampict <anampict@gmail.com>
2025-07-18 13:04:40 +0700

    menambahkan fungsi ambil semua data dan hapus data berdasarkan id

--- controllers/product_controller.go
+++ controllers/product_controller.go
@@ -3,10 +3,12 @@ package controllers
 import (
 	"backendGalonku/config"
 	"backendGalonku/models"
+	"context"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -36,4 +38,48 @@ func CreateProduct(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Produk berhasil ditambahkan", "product": product})
 
+}
+
+//fungsi untuk mendapatkan semua produk
+
+func GetAllProducts(c *gin.Context) {
+	var products[]models.Product
+
+	cursor, err := productCollection.Find(context.TODO(),bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "gagal mendapatkan produk"})
+		return
+	}
+	defer cursor.Close(context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		var product models.Product
+		if err := cursor.Decode(&product); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "gagal decode produk"})
+			return
+		}
+		products = append(products, product)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "berhasil mendapatkan produk", "products": products})
+}
+
+
+// fungsi untuk menghapus produk berdasarkan ID
+
+func DeleteProduct(c *gin.Context) {
+	idParam := c.Param("id")
+
+	objID, err := primitive.ObjectIDFromHex(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID tidak valid"})
+		return
+	}
+
+	_, err = productCollection.DeleteOne(context.TODO(), bson.M{"_id": objID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "gagal menghapus produk"})
+		return
+	}
 }
